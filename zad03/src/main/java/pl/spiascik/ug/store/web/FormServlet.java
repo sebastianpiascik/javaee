dd029a3a69c77bd8c091ce09dc69f772108b1458
Sebastian <sebastian.anonim97@gmail.com>
2018-11-07 19:13:42 +0100

    session fix

--- zad03/src/main/java/pl/spiascik/ug/store/web/FormServlet.java
+++ zad03/src/main/java/pl/spiascik/ug/store/web/FormServlet.java
@@ -9,15 +9,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 @WebServlet("/form")
 public class FormServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
--- zad03/src/main/java/pl/spiascik/ug/store/web/ShopServlet.java
+++ zad03/src/main/java/pl/spiascik/ug/store/web/ShopServlet.java
@@ -9,17 +9,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
 
 @WebServlet("/")
 public class ShopServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession(true);
+
+        ArrayList<Cloth> clothes;
+        String clothesKey = new String("clothes");
+
+
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
-        if(request.getParameter("formName").equals("addToBasket")){
-            ShopService.addToBasket(Integer.parseInt(request.getParameter("id")));
+        if (request.getParameter("formName").equals("addToBasket")) {
+
+
+            if (!session.isNew()) {
+                clothes = (ArrayList<Cloth>) session.getAttribute(clothesKey);
+                clothes.add(ClothService.getCloth(Integer.parseInt(request.getParameter("id"))));
+                session.setAttribute(clothesKey, clothes);
+            }
+
+
+//            ShopService.addToBasket(Integer.parseInt(request.getParameter("id")));
 
             try {
                 out.println("<!DOCTYPE html>");
@@ -37,8 +56,24 @@ public class ShopServlet extends HttpServlet {
             }
         }
 
-        if(request.getParameter("formName").equals("removeFromBasket")){
-            ShopService.removeFromBasket(Integer.parseInt(request.getParameter("id")));
+        if (request.getParameter("formName").equals("removeFromBasket")) {
+
+            if (!session.isNew()) {
+                clothes = (ArrayList<Cloth>) session.getAttribute(clothesKey);
+                clothes.remove(ClothService.getCloth(Integer.parseInt(request.getParameter("id"))));
+
+//                int id = Integer.parseInt(request.getParameter("id"));
+//                for (Cloth cloth : clothes) {
+//                    if (cloth.getId() == id) {
+//                        clothes.remove(id);
+//                        break;
+//                    }
+//                }
+                session.setAttribute(clothesKey, clothes);
+            }
+
+
+//            ShopService.removeFromBasket(Integer.parseInt(request.getParameter("id")));
 
             try {
                 out.println("<!DOCTYPE html>");
@@ -60,6 +95,22 @@ public class ShopServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        HttpSession session = request.getSession(true);
+
+        ArrayList<Cloth> clothes = new ArrayList<Cloth>();
+        String clothesKey = new String("clothes");
+
+        // Check if this is new comer on your web page.
+        if (session.isNew()) {
+
+        } else {
+            clothes = (ArrayList<Cloth>) session.getAttribute(clothesKey);
+        }
+        session.setAttribute(clothesKey, clothes);
+
+
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
@@ -82,19 +133,39 @@ public class ShopServlet extends HttpServlet {
                             "<br/>Data dodania: <i>" + cloth.getProductionDate() + "</i></li>");
                     out.println("<form action='' method='post'>" +
                             "<input type='hidden' name='formName' value='addToBasket' />" +
-                            "<input type='text' name='id' value='"+ cloth.getId() +"' hidden />" +
+                            "<input type='text' name='id' value='" + cloth.getId() + "' hidden />" +
                             "<button type='submit'>Dodaj do koszyka</button><br/><br/>" +
                             "</form>");
                 }
                 out.println("</ul>");
-            } else{
+            } else {
                 out.println("<p>Brak produktów w sklepie</p>");
             }
             out.println("<p><a href='/zad03/form'>Dodaj produkt</a></p>");
-            if (ClothService.getAllClothes().size() > 0) {
+//            if (ClothService.getAllClothes().size() > 0) {
+//                out.println("<h3>Produkty w koszyku:</h3>");
+//                out.println("<ul>");
+//                for (Cloth cloth : ShopService.getBasket()) {
+//                    String waterproof = "";
+//                    if (cloth.isWaterproof())
+//                        waterproof = " - wodoodporność";
+//                    out.println("<li>Produkt: " + cloth.getName() + waterproof +
+//                            "<br/>Cena: " + cloth.getPrice() + "zł" +
+//                            "<br/>Data dodania: <i>" + cloth.getProductionDate() + "</i></li>");
+//                    out.println("<form action='' method='post'>" +
+//                            "<input type='hidden' name='formName' value='removeFromBasket' />" +
+//                            "<input type='text' name='id' value='" + cloth.getId() + "' hidden />" +
+//                            "<button type='submit'>Usuń z koszyka</button><br/><br/>" +
+//                            "</form>");
+//                }
+//                out.println("</ul>");
+//            } else {
+//                out.println("<p>Brak produktów w koszyku</p>");
+//            }
+            if (clothes.size() > 0) {
                 out.println("<h3>Produkty w koszyku:</h3>");
                 out.println("<ul>");
-                for (Cloth cloth : ShopService.getBasket()) {
+                for (Cloth cloth : clothes) {
                     String waterproof = "";
                     if (cloth.isWaterproof())
                         waterproof = " - wodoodporność";
@@ -103,12 +174,12 @@ public class ShopServlet extends HttpServlet {
                             "<br/>Data dodania: <i>" + cloth.getProductionDate() + "</i></li>");
                     out.println("<form action='' method='post'>" +
                             "<input type='hidden' name='formName' value='removeFromBasket' />" +
-                            "<input type='text' name='id' value='"+ cloth.getId() +"' hidden />" +
+                            "<input type='text' name='id' value='" + cloth.getId() + "' hidden />" +
                             "<button type='submit'>Usuń z koszyka</button><br/><br/>" +
                             "</form>");
                 }
                 out.println("</ul>");
-            } else{
+            } else {
                 out.println("<p>Brak produktów w koszyku</p>");
             }
             out.println("</body>");
